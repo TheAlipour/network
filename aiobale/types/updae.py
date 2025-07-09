cfabbb456a448ec2de8f5c1d83a9cff066ca8766
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-09 15:27:30 +0330

    Update now gives the event_type and active event

--- aiobale/types/updae.py
+++ aiobale/types/updae.py
@@ -1,5 +1,6 @@
+from functools import cached_property
 from pydantic import Field
-from typing import Any, Optional
+from typing import Any, Optional, Tuple
 
 from .base import BaleObject
 from .message import Message
@@ -7,6 +8,14 @@ from .message import Message
 
 class Update(BaleObject):
     message: Optional[Message] = Field(None, alias="55")
+    
+    @cached_property
+    def current_event(self) -> Optional[Tuple[str, Any]]:
+        for field_name in self.__annotations__:
+            value = getattr(self, field_name, None)
+            if value is not None:
+                return field_name, value
+        return None
 
 
 class UpdateBody(BaleObject):
