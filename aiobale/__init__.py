90364dfbeedd7f2da01d39a52979405a89ea32a9
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-09 19:45:22 +0330

    Added Client, Dispatcher and Router in __init__

---
+++ aiobale/__init__.py
@@ -0,0 +1,10 @@
+from .client.client import Client
+from .dispatcher.dispatcher import Dispatcher
+from .dispatcher.router import Router
+
+
+__all__ = (
+    "Client",
+    "Dispatcher",
+    "Router"
+)
