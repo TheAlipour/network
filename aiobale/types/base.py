ac7150eb0a6c90d79c579e8f60a410efd6b5db07
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-11 02:39:02 +0330

    Now nothing is frozen

--- aiobale/types/base.py
+++ aiobale/types/base.py
@@ -8,7 +8,6 @@ class BaleObject(BotContextController, BaseModel):
         populate_by_name=True,
         use_enum_values=True,
         extra="allow",
-        frozen=True,
         validate_assignment=True,
         arbitrary_types_allowed=True,
         defer_build=True,
