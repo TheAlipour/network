ba1f320428ce6654389e4d01779880fc862e069a
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-11 17:19:15 +0330

    Fixed history response

--- aiobale/types/responses/history.py
+++ aiobale/types/responses/history.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from pydantic import Field
-from typing import List
+from pydantic import Field, model_validator
+from typing import List, Dict, Any
 
 from ..message_data import MessageData
 from ..base import BaleObject
@@ -10,6 +10,13 @@ from ..chat import Chat
 
 class HistoryResponse(BaleObject):
     result: List[MessageData] = Field(..., alias="1")
+    
+    @model_validator(mode="before")
+    def validate_list(cls, data: Dict[str, Any]):
+        if not isinstance(data["1"], dict):
+            data["1"] = list(data["1"])
+        
+        return data
     
     def _add_chat_to_message(self, message: MessageData, chat: Chat):
         message.chat = chat
