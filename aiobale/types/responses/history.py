157f503537b92c657f9c32636def79457d2dc06c
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-11 17:41:48 +0330

    Fixed some bugs

--- aiobale/types/responses/history.py
+++ aiobale/types/responses/history.py
@@ -18,11 +18,8 @@ class HistoryResponse(BaleObject):
         
         return data
     
-    def _add_chat_to_message(self, message: MessageData, chat: Chat):
-        message.chat = chat
-        if message.replied_to is not None:
-            self._add_chat_to_message(message.replied_to, chat)
-
     def add_chat(self, chat: Chat) -> None:
         for message in self.result:
-            self._add_chat_to_message(message, chat)
+            message.chat = chat
+            if message.replied_to is not None:
+                message.replied_to.chat = chat
