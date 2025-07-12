83bc9d50295742603592a64df1d5f551d1728bdc
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-12 15:17:10 +0330

    Added PinMessage method

--- aiobale/methods/messaging/pin_message.py
+++ aiobale/methods/messaging/pin_message.py
@@ -1,22 +1,22 @@
 from pydantic import Field, model_validator
-from typing import TYPE_CHECKING, Any, Dict, List
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
-from ...types import Peer, IntValue, BytesValue, InfoMessage
+from ...types import Peer, InfoMessage, OtherMessage, IntBool
 from ...utils import Int64VarintCodec
 from ...types.responses import DefaultResponse
 from ...enums import Services
 from ..base import BaleMethod
 
 
-class ForwardMessages(BaleMethod):
+class PinMessage(BaleMethod):
     __service__ = Services.MESSAGING.value
-    __method__ = "ForwardMessages"
+    __method__ = "PinMessage"
     
     __returning__ = DefaultResponse
     
     peer: Peer = Field(..., alias="1")
-    message_ids: bytes = Field(..., alias="2")
-    forwarded_messages: List[InfoMessage] = Field(..., alias="3")
+    message: OtherMessage = Field(..., alias="2")
+    just_me: Optional[IntBool] = Field(None, alias="3")
     
     if TYPE_CHECKING:
         # Just For Type Helping
@@ -25,21 +25,15 @@ class ForwardMessages(BaleMethod):
             __pydantic__self__,
             *,
             peer: Peer,
-            message_ids: List[int],
-            forwarded_messages: List[InfoMessage],
+            message: OtherMessage,
+            just_me: bool,
             **__pydantic_kwargs: Any
         ) -> None:
             # Is needed only for type checking and IDE support without any additional plugins
             
             super().__init__(
                 peer=peer,
-                message_ids=message_ids,
-                forwarded_messages=forwarded_messages,
+                message=message,
+                just_me=just_me,
                 **__pydantic_kwargs
             )
-            
-    @model_validator(mode="before")
-    @classmethod
-    def fix_lists(cls, data: Dict[str, Any]) -> Dict[str, Any]:
-        data["message_ids"] = Int64VarintCodec.encode_list(data["message_ids"])
-        return data
