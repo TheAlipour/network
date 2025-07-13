d32703638b2df9628c514df1b146b187f38737db
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-14 01:43:07 +0330

    Added FullUser

--- aiobale/enums/__init__.py
+++ aiobale/enums/__init__.py
@@ -11,4 +11,5 @@ __all__ = (
     "Services",
     "SendCodeType",
     "ListLoadMode",
+    "PrivacyMode",
 )
--- aiobale/types/full_user.py
+++ aiobale/types/full_user.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 from pydantic import Field
 
-from ..enums import ChatType
+from ..enums import ChatType, PrivacyMode
 from .int_bool import IntBool
 from .base import BaleObject
 
@@ -15,6 +15,11 @@ class FullUser(BaleObject):
     id: int = Field(..., alias="1")
     about: Optional[str] = Field(None, alias="3")
     languages: Optional[List[str]] = Field(None, alias="4")
+    timezone: Optional[str] = Field(None, alias="5")
+    is_blocked: IntBool = Field(False, alias="6")
     ex_info: ExInfo = Field(..., alias="9")
+    is_deleted: IntBool = Field(False, alias="12")
+    is_contact: IntBool = Field(False, alias="13")
     created_at: int = Field(..., alias="15")
-    privacy_mode: int = Field(..., alias="16")
+    privacy_mode: PrivacyMode = Field(..., alias="16")
+    allowed_invite: IntBool = Field(False, alias="17")
