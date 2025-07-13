515ee8ff548d43161727d689c1f34fecd80fb415
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-14 02:18:59 +0330

    Fixed a bug

--- aiobale/types/full_user.py
+++ aiobale/types/full_user.py
@@ -27,15 +27,16 @@ class FullUser(BaleObject):
     @model_validator(mode="before")
     @classmethod
     def fix_fields(cls, data: Dict[str, Any]) -> Dict[str , Any]:
-        for key, value in data.items():
-            if isinstance(value, dict) \
-                and len(value) == 1 and "1" in value:
-                    data[key] = value["1"]
-                    
+        for key in list(data.keys()):
+            value = data[key]
+
+            if isinstance(value, dict) and len(value) == 1 and "1" in value:
+                data[key] = value["1"]
+
             elif not value:
                 data.pop(key)
-                    
-            if key == "4" and not isinstance(value, List):
-                data[key] = [value]
-                
+
+            elif key == "4" and not isinstance(data[key], list):
+                data[key] = [data[key]]
+
         return data
