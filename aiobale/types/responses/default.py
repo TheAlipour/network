ff1e9ec3bed98d98220b3dbcb2fcb415d5e7bde2
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-14 22:46:21 +0300

    Removed an annoying field

--- aiobale/types/responses/default.py
+++ aiobale/types/responses/default.py
@@ -8,4 +8,3 @@ from ..base import BaleObject
 class DefaultResponse(BaleObject):
     seq: Optional[int] = Field(None, alias="1")
     data: Optional[int] = Field(None, alias="2")
-    ext: Optional[List[ExtData]] = Field(None, alias="4")
