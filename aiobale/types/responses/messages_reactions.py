da1a045780f8eb112e32ee14e55da094abcf7315
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-18 08:56:01 +0330

    Fixed a bug

--- aiobale/types/responses/messages_reactions.py
+++ aiobale/types/responses/messages_reactions.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from pydantic import Field, ValidationInfo, model_validator
-from typing import List, Optional, Any, Dict, TYPE_CHECKING
+from pydantic import Field, model_validator
+from typing import List, Any, Dict
 
 from ..base import BaleObject
 from ..message_reaction import MessageReactions
@@ -9,3 +9,14 @@ from ..message_reaction import MessageReactions
 
 class ReactionsResponse(BaleObject):
     messages: List[MessageReactions] = Field([], alias="1")
+    
+    @model_validator(mode="before")
+    @classmethod
+    def validate_list(cls, data: Dict[str, Any]) -> Dict[str, Any]:
+        if "1" not in data:
+            return data
+        
+        if not isinstance(data["1"], list):
+            data["1"] = [data["1"]]
+        
+        return data
