baaabd37fd6948986c3525a35ba8064a81e6dbbf
AlirezaJahaniBiglar <alirezajahani20092009@gmail.com>
2025-07-19 20:33:05 +0330

    Fixed some bugs

--- aiobale/types/info_changed.py
+++ aiobale/types/info_changed.py
@@ -1,16 +1,19 @@
 from pydantic import Field, model_validator
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from .base import BaleObject
 
 
 class UsernameChanged(BaleObject):
     user_id: int = Field(..., alias="1")
-    username: str = Field(..., alias="2")
+    username: Optional[str] = Field(None, alias="2")
     
     @model_validator(mode="before")
     @classmethod
     def fix_fields(cls, data: Dict[str, Any]) -> Dict[str, Any]:
+        if "2" not in data:
+            return data
+        
         data["2"] = data["2"]["1"]
         
         return data
